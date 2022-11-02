c91e444f19b06dce9b6f37490c44558ca79840f2
udaykirankolla <uday7702192162@gmail.com>
2022-11-02 16:01:04 -0500

    added bonus

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # f2wb84kolla
 # Hai this is UdayKiran Kolla
 #webapps assignment
+Class Name : Application
+Attributes : Door_Name(String), Door_Company(String), Door_Size(number)
+> __Class Name__ : Application 
+> __Attributes__ : *Door_Name(String), Door_Company(String), Door_Size(number)*
+## [My Render Link](https://f2wb84kolla.onrender.com)
--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var doorRouter = require('./routes/door');
+var gridbulidRouter = require('./routes/gridbulid');
+var selectorRouter = require('./routes/selector')
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/door',doorRouter);
+app.use('/gridbulid',gridbulidRouter);
+app.use('/selector',selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
